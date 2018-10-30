63f1df8d42fdae53cbf9c97bf5af653386106b36
glauber silva <glauber.lucio.silva@gmail.com>
2018-10-30 12:02:46 -0300

    Added comments

--- 002-var-keywords/varkeywords.go
+++ 002-var-keywords/varkeywords.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 var y = 43
+// DECLARE there is a VARIABLE with IDENTIFIER "z"
+// and that the VARIABLE with the IDENTIFIER "z" is of TYPE Int
+// ASSIGNS the  ZERO VALUE of TYPE int to "z"
+var z int
 
 func main() {
 	//short declaration operator
@@ -12,4 +16,5 @@ func main() {
 	fmt.Println(x)
 
 	fmt.Println(y)
+	fmt.Println(z)
 }
